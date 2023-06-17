14bd7b5e366f09be116c5603847c2ddf7761e1ca
Faridl Mukhlashin Akbarullah <41612608+ak8ar12ullah@users.noreply.github.com>
2023-06-17 21:30:01 +0700

    menambah halaman testimoni

--- frontend/PWA/src/scripts/views/pages/testimoni.js
+++ frontend/PWA/src/scripts/views/pages/testimoni.js
@@ -1,6 +1,45 @@
 const Testimoni = {
   async render() {
-    return ``;
+    return `
+        <div class="testimoni">
+            <div class="judul">
+                <h1>Ulasan Pengguna Share2Care</h1>
+                <p>Kisah Inspiratif Pengguna Share2Care Mewarnai Dunia dengan Donasi dan Kebaikan</p>
+            </div>
+            <div class="listTestimoni">
+                <div class="card">
+                    <img src="./fitur.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros vitae nisl aliquam. Sed vitae eros vitae nisl aliquam</p>
+                    <h3>John Doe</h3>
+                </div>
+                <div class="card">
+                    <img src="./fitur.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros vitae nisl aliquam. Sed vitae eros vitae nisl aliquam</p>
+                    <h3>John Doe</h3>
+                </div>
+                <div class="card">
+                    <img src="./fitur.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros vitae nisl aliquam. Sed vitae eros vitae nisl aliquam</p>
+                    <h3>John Doe</h3>
+                </div>
+                <div class="card">
+                    <img src="./fitur.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros vitae nisl aliquam. Sed vitae eros vitae nisl aliquam</p>
+                    <h3>John Doe</h3>
+                </div>
+                <div class="card">
+                    <img src="./fitur.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros vitae nisl aliquam. Sed vitae eros vitae nisl aliquam</p>
+                    <h3>John Doe</h3>
+                </div>
+                <div class="card">
+                    <img src="./fitur.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros vitae nisl aliquam. Sed vitae eros vitae nisl aliquam</p>
+                    <h3>John Doe</h3>
+                </div>
+            </div>
+        </div>
+    `;
   },
   async afterRender() {},
 };
