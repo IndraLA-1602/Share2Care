19d94b4c8e32df01649a6f255d9124d6d27d0916
Lutfi-77 <xenonoffic@gmail.com>
2023-06-19 11:33:11 +0700

    update about us

--- frontend/PWA/src/scripts/component/AboutUs.js
+++ frontend/PWA/src/scripts/component/AboutUs.js
@@ -1,10 +1,10 @@
-import styleR from "./styleShadow/styleR.js";
-import responsiveStyle from "./styleShadow/styleResponsive.js";
+import styleR from './styleShadow/styleR.js';
+import responsiveStyle from './styleShadow/styleResponsive.js';
 
 class AboutUs extends HTMLElement {
   constructor() {
     super();
-    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
   }
   connectedCallback() {
     this.render();
@@ -59,7 +59,7 @@ class AboutUs extends HTMLElement {
         </div>
         <div class="profile-card">
           <div class="profile-picture">
-            <img src="./indra.png" alt="">
+            <img src="./dawan_lutfi.jpg" alt="">
           </div>
           <div class="profile-info">
             <h3 class="profile-name">Mohammad Dawan Lutfi</h3>
@@ -104,41 +104,41 @@ class AboutUs extends HTMLElement {
       `;
 
     const checkTeamCardVisibility = () => {
-      const teamCards = this._shadowRoot.querySelectorAll(".profile-card");
-      const fiturList = this._shadowRoot.querySelectorAll(".Fitur_list");
-      const productCards = this._shadowRoot.querySelectorAll(".product-cards");
+      const teamCards = this._shadowRoot.querySelectorAll('.profile-card');
+      const fiturList = this._shadowRoot.querySelectorAll('.Fitur_list');
+      const productCards = this._shadowRoot.querySelectorAll('.product-cards');
 
       teamCards.forEach((card) => {
         const rect = card.getBoundingClientRect();
         if (rect.top < window.innerHeight) {
-          card.classList.add("visible");
+          card.classList.add('visible');
         }
       });
       fiturList.forEach((card) => {
         const rect = card.getBoundingClientRect();
         if (rect.top < window.innerHeight) {
-          card.classList.add("visible");
+          card.classList.add('visible');
         }
       });
       productCards.forEach((card) => {
         const rect = card.getBoundingClientRect();
         if (rect.top < window.innerHeight) {
-          card.classList.add("visible");
+          card.classList.add('visible');
         }
       });
     };
 
-    window.addEventListener("hashchange", () => {
+    window.addEventListener('hashchange', () => {
       checkTeamCardVisibility();
     });
 
-    window.addEventListener("load", () => {
+    window.addEventListener('load', () => {
       checkTeamCardVisibility();
     });
 
-    window.addEventListener("scroll", checkTeamCardVisibility);
-    window.addEventListener("resize", checkTeamCardVisibility);
+    window.addEventListener('scroll', checkTeamCardVisibility);
+    window.addEventListener('resize', checkTeamCardVisibility);
   }
 }
 
-customElements.define("about-us", AboutUs);
+customElements.define('about-us', AboutUs);
